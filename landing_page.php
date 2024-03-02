33d27d361b37ac193d07a2e005b5ae9ff0a2bd45
ApexMale <108138836+Loophole07@users.noreply.github.com>
2024-03-02 21:50:54 +0545

    Add files via upload

---
+++ landing_page.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CarBUYsell</title>
+    <link rel="stylesheet" type="text/css" href="landingpage.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+   
+</head>
+<body>
+    <div>
+    <nav>
+        <ul>
+        <li><a href=""><i class="fa-solid fa-bars"></i></a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="login_form.php">Login</a></li>
+        <li><a href="registration_form.php">Register</a></li>
+        <li><a href="AboutUs.php">About Us</a></li>
+        <li><a href="contactus.php">Contact Us</a></li>
+        </ul>
+    </nav>
+    <div class="background-container">
+    <div class="container">
+        <p>You see it, You like it, You want it, You got it.</p>
+        <p> Ready to start a new adventure with my YOUR own car</p>
+</div>
+    </div>
+</div>
+</body>
+</html>
